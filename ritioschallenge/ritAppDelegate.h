1244475c0edb913b06ece2af798b45a489cdc5d4
jonathan <jml6757@rit.edu>
2014-01-30 20:36:36 -0500

    asdfasdf

---
+++ ritioschallenge/ritAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  ritAppDelegate.h
+//  ritioschallenge
+//
+//  Created by Student on 1/30/14.
+//  Copyright (c) 2014 Student. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ritAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
